7af1993315d94f2b301ca0e6722fd1382b74495a
Kutu <125172829+KutulloR@users.noreply.github.com>
2024-05-05 22:17:41 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['35.174.185.85', '100.25.222.44']
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
