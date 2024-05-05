7769697c9eafe896bfaef5078f67156b5e9db06a
Kutu <125172829+KutulloR@users.noreply.github.com>
2024-05-05 22:18:40 +0200

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 Deletes out-of-date archives
@@ -8,7 +9,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['35.174.185.85', '100.25.222.44']
+env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
